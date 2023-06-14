75136d0bae34783b3b1281fe53c575acd31937ed
uday <uday.p@simformsolutions.com>
2023-06-14 12:43:05 +0530

    added navbar stylings and Home page

--- HRMS/src/views/Home.vue
+++ HRMS/src/views/Home.vue
@@ -22,12 +22,32 @@
 .home-head {
     font-size: 100px;
 }
-h1{
-    font-weight:1000;
+
+h1 {
+    font-weight: 1000;
 }
+
 .usr-name {
     background: -webkit-linear-gradient(#09C6F9, #045DE9);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+}
+
+@media (max-width:700px) {
+    .home-head {
+        font-size: 80px !important;
+    }
+}
+
+@media (max-width:550px) {
+    .home-head {
+        font-size: 60px !important;
+    }
+}
+
+@media (max-width:350px) {
+    .home-head {
+        font-size: 50px !important;
+    }
 }
 </style>
