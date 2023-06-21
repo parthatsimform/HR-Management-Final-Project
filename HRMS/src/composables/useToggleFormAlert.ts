a0800bf5cb44b10871f8fe364662f42fab4b01a7
Parth Dedaniya <parth.dedaniya@simformsolutions.com>
2023-06-21 11:00:27 +0530

    Change in dispaly and remove alert function

--- HRMS/src/composables/useToggleFormAlert.ts
+++ HRMS/src/composables/useToggleFormAlert.ts
@@ -1,19 +1,21 @@
 export const useToggleFormAlert = () => {
-    const displayAlert = (el:HTMLFormElement, msg:string):boolean => {
-        el.style = "border : 1px solid red";
-        const alertElClass : string = `${el.id}Err`;
-        const errEl = document.querySelector(`.${alertElClass}`) as HTMLElement
-        errEl!.style = "color : red"
-        errEl!.textContent = `${msg}`;
-        return false;
-    };
+	const displayAlert = (el: HTMLFormElement, msg: string): boolean => {
+		el.style.setProperty("border", "1px solid red", "important");
+		const alertElClass: string = `${el.id}Err`;
+		const errEl = document.querySelector(`.${alertElClass}`) as HTMLElement;
+		errEl.style.setProperty("color", "red");
+		errEl!.textContent = `${msg}`;
+		return false;
+	};
 
-    const removeAlert = (el:HTMLFormElement) => {
-        el.style.border = "1px solid #c5c5c5";
-        const alertElClass : string = `${el.id}Err`;
-        document.querySelector(`.${alertElClass}`)!.textContent = "";
-        return true;
-    };
+	const removeAlert = (el: HTMLFormElement) => {
+		el.style.removeProperty("border");
+		const alertElClass: string = `${el.id}Err`;
+		const errEl = document.querySelector(`.${alertElClass}`) as HTMLElement;
+		errEl.textContent = "";
+		errEl.style.removeProperty("color");
+		return true;
+	};
 
 	return { displayAlert, removeAlert };
 };
